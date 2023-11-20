ed2cb88eeaf7a307a67366903380592b2f891758
f.trotter <felix.trotter86@gmail.com>
2023-11-20 13:13:47 +0000

    Create EMEC_PAM_models.py

---
+++ EMEC_PAM_models.py
@@ -0,0 +1,9 @@
+
+
+### This script explores models to classify underwater acoustic data to 
+### identify marine life
+
+
+
+## ==================================================================
+###
