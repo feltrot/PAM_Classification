eb2514ca0c8ab22b52192ce9c17562807d4352ac
f.trotter <felix.trotter86@gmail.com>
2023-11-21 11:17:55 +0000

    Update EMEC_PAM_models.py

--- EMEC_PAM_models.py
+++ EMEC_PAM_models.py
@@ -23,33 +23,59 @@
 # also install all necessary modules for analyses, such as numpy, pandas etc.
 
 
-
-
 ######################################################################
 # now lets start coding :)
-# we already have a depository with tensorflow called ketos, following this 
-# webpage on classifying whale calls in North America
-# https://docs.meridian.cs.dal.ca/ketos/tutorials/create_database_simpler/index.html
-# it is a very good tutorial on how to create training data and using a 
-# classfifier model to identify whale calls
+
+# the code below is from Mike Polinowksi (see link above)
 
 
 
 # import modules to jupyter notebook
 import os
 from matplotlib import pyplot as plt
-import tensorflow as tf 
-
-
+import tensorflow as tf
+import tensorflow_io as tfio
 
 
 
 # Data Loading
 # ========================
 
-
 # To work with the recorded audio files we can select the corresponding data paths - in the example below 
 # we pick both an example file that contains the signal we are looking for and one that only comes with background noise:
+HARBORPORP_FILE = os.path.join('Documents', 'EMEC', 'Acoustics', 'Data_Acoustics', 'harborPorpoise.wav')
+NOT_HARBORPORP_FILE = os.path.join('Documents', 'EMEC', 'Acoustics', 'Data_Acoustics', '3channel_sample_data_fromEMEC', 'channelABC_2023-07-25_14-09-33.wav')
 
-CAPUCHIN_FILE = os.path.join('data', 'positives', 'XC3776-3.wav')
-NOT_CAPUCHIN_FILE = os.path.join('data', 'negatives', 'afternoon-birds-song-in-forest-0.wav')
+# And read a single audio stream (mono) resampled to 16kHz from those files by feeding the filepath into the following function:
+    # - decode_wav : Read single channel from stereo file
+    # - squeeze : Since all the data is single channel (mono), drop the channels axis from array
+    # - resample : Reduce audio data to 16kHz
+
+
+def load_wav_16k_mono(filename):
+    # Load encoded wav file
+    file_contents = tf.io.read_file(filename)
+    # Decode wav (tensors by channels) 
+    wav, sample_rate = tf.audio.decode_wav(file_contents, desired_channels=1)
+    # Removes trailing axis
+    wav = tf.squeeze(wav, axis=-1)
+    sample_rate = tf.cast(sample_rate, dtype=tf.int64)
+    # Goes from 44100Hz to 16000hz - amplitude of the audio signal
+    wav = tfio.audio.resample(wav, rate_in=sample_rate, rate_out=16000)
+    return wav
+
+
+# We can run the function over both the positive and negative file:
+wave = load_wav_16k_mono(HARBORPORP_FILE)
+nwave = load_wav_16k_mono(NOT_HARBORPORP_FILE)
+
+
+file_contents = tf.io.read_file(HARBORPORP_FILE)
+# Decode wav (tensors by channels) 
+wav, sample_rate = tf.audio.decode_wav(file_contents, desired_channels=1)
+# Removes trailing axis
+wav = tf.squeeze(wav, axis=-1)
+sample_rate = tf.cast(sample_rate, dtype=tf.int64)
+# Goes from 44100Hz to 16000hz - amplitude of the audio signal
+wav = tfio.audio.resample(wav, rate_in=sample_rate, rate_out=16000)
+return wav
